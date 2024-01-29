32b53097ebf9ff039da14c4c53d53f5f04deb268
Kevin Lopez <kevinjeff04@gmail.com>
2024-01-28 22:24:30 -0500

    feat: :construction: add initial router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/about";
+
 const App = () => {
   return (
-    <div id="main">
-      <h1>React App</h1>
-    </div>
+    <BrowserRouter>
+      <h1>Home</h1>
+      <Routes>
+        <Route path="/" element={<Link to="/about">About Me</Link>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
